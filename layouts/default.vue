5f88e1d5309d92eb8e0e044ff57d1d18e3cc92aa
disguy-droid <Monishatom@outlook.com>
2022-09-18 11:47:53 +0530

    fix: component render duplicate file

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,21 +2,34 @@
 import { useStore } from "~~/store/state";
 
 const store = useStore();
-const nuxtApp = useNuxtApp();
+// const nuxtApp = useNuxtApp();
 
-nuxtApp.hook("page:finish", () => {
-  window.scrollTo(0, 0);
-});
+// nuxtApp.hook("page:finish", () => {
+//   window.scrollTo(0, 0);
+// });
 </script>
 
 <template>
-  <div
-    :class="{ 'bg-black': store.isDark, 'bg-white': store.isLight }"
-    class="transition-color duration-200 ease-linear"
-  >
-    <Header />
-    <slot />
-  </div>
+  <main>
+    <Html>
+      <Head>
+        <Title>Nuxt App</Title>
+        <Link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="../assets/favicon-32x32.png"
+        />
+      </Head>
+    </Html>
+    <div
+      :class="{ 'bg-black': store.isDark, 'bg-white': store.isLight }"
+      class="transition-color duration-200 ease-linear"
+    >
+      <Header />
+      <slot />
+    </div>
+  </main>
 </template>
 
 <style scoped></style>
